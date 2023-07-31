89074fc4918348643051e6f254f35c67e71f10b5
Lucas-b-o <140469984+Lucas-b-o@users.noreply.github.com>
2023-07-31 16:44:31 -0300

    2.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,4 @@
-const Header = ({ course }) => <h3>{course}</h3>
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map((part) => <Part part={part} key={part.name} />)}
-  </>
-
-const Total = ({ parts }) =>
-  <>
-    <p><strong>total of {
-      parts.reduce((sum, part) => {
-        return sum + part.exercises
-      }, 0)
-    } exercises</strong></p>
-  </>
-
-
-const Course = ({ course, parts }) =>
-  <>
-    <Header course={course} />
-    <Content parts={parts} />
-    <Total parts={parts} />
-  </>
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
