acfc9f374ceb446088b376e2e7313bb43fbceb69
Lucas-b-o <140469984+Lucas-b-o@users.noreply.github.com>
2023-08-01 16:06:50 -0300

    2.18

--- src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = ({handleFilterNameChange, filterName}) => {
-  return (
-    <div>filter show with <input onChange={handleFilterNameChange} value={filterName} /></div>
-  )
-}
-
-export default Filter
